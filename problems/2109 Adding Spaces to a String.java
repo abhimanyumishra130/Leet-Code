b44e3872ea2ac716ae94dbbc512450fe38aa02bb
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2024-12-03 09:51:59 +0530

    Create 2109 Adding Spaces to a String.java

---
+++ problems/2109 Adding Spaces to a String.java
@@ -0,0 +1,89 @@
+/*
+You are given a 0-indexed string s and a 0-indexed integer array spaces that describes the indices in the original string where spaces will be added. Each space should be inserted before the character at the given index.
+
+For example, given s = "EnjoyYourCoffee" and spaces = [5, 9], we place spaces before 'Y' and 'C', which are at indices 5 and 9 respectively. Thus, we obtain "Enjoy Your Coffee".
+Return the modified string after the spaces have been added.
+
+ 
+
+Example 1:
+
+Input: s = "LeetcodeHelpsMeLearn", spaces = [8,13,15]
+Output: "Leetcode Helps Me Learn"
+Explanation: 
+The indices 8, 13, and 15 correspond to the underlined characters in "LeetcodeHelpsMeLearn".
+We then place spaces before those characters.
+Example 2:
+
+Input: s = "icodeinpython", spaces = [1,5,7,9]
+Output: "i code in py thon"
+Explanation:
+The indices 1, 5, 7, and 9 correspond to the underlined characters in "icodeinpython".
+We then place spaces before those characters.
+Example 3:
+
+Input: s = "spacing", spaces = [0,1,2,3,4,5,6]
+Output: " s p a c i n g"
+Explanation:
+We are also able to place spaces before the first character of the string.
+ 
+
+Constraints:
+
+1 <= s.length <= 3 * 105
+s consists only of lowercase and uppercase English letters.
+1 <= spaces.length <= 3 * 105
+0 <= spaces[i] <= s.length - 1
+All the values of spaces are strictly increasing.
+*/
+
+
+// 2273 ms approach
+
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder res = new StringBuilder(s);
+        int count = 0;
+        for(int i=0; i< spaces.length; i++){
+            res.insert(spaces[i]+count, " ");
+            count++;
+        }
+        return res.toString();
+    }
+}
+
+// Optimized solution
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder res = new StringBuilder("");
+        for(int i=0, ind=0; i< s.length(); i++){
+            if(ind<spaces.length && spaces[ind] == i){
+                res.append(" ");
+                ind++;
+            }
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+}
+
+// more optimized
+
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder res = new StringBuilder("");
+        for(int i=0, ind=0; i< s.length(); i++){
+            if(spaces[ind] == i){
+                res.append(" ");
+                ind++;
+            }
+            if(ind>spaces.length-1){
+                res.append(s.substring(i));
+                i=s.length();
+                continue;
+            }
+            res.append(s.charAt(i));
+        }
+        return res.toString();
+    }
+}
