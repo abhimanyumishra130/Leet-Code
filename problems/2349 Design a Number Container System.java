54dc6ee3a2d8f59f70264558fe826bd8ccdd141c
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2025-02-08 15:57:23 +0530

    Create 2349 Design a Number Container System.java

---
+++ problems/2349 Design a Number Container System.java
@@ -0,0 +1,82 @@
+/*
+Design a number container system that can do the following:
+
+Insert or Replace a number at the given index in the system.
+Return the smallest index for the given number in the system.
+Implement the NumberContainers class:
+
+NumberContainers() Initializes the number container system.
+void change(int index, int number) Fills the container at index with the number. If there is already a number at that index, replace it.
+int find(int number) Returns the smallest index for the given number, or -1 if there is no index that is filled by number in the system.
+ 
+
+Example 1:
+
+Input
+["NumberContainers", "find", "change", "change", "change", "change", "find", "change", "find"]
+[[], [10], [2, 10], [1, 10], [3, 10], [5, 10], [10], [1, 20], [10]]
+Output
+[null, -1, null, null, null, null, 1, null, 2]
+
+Explanation
+NumberContainers nc = new NumberContainers();
+nc.find(10); // There is no index that is filled with number 10. Therefore, we return -1.
+nc.change(2, 10); // Your container at index 2 will be filled with number 10.
+nc.change(1, 10); // Your container at index 1 will be filled with number 10.
+nc.change(3, 10); // Your container at index 3 will be filled with number 10.
+nc.change(5, 10); // Your container at index 5 will be filled with number 10.
+nc.find(10); // Number 10 is at the indices 1, 2, 3, and 5. Since the smallest index that is filled with 10 is 1, we return 1.
+nc.change(1, 20); // Your container at index 1 will be filled with number 20. Note that index 1 was filled with 10 and then replaced with 20. 
+nc.find(10); // Number 10 is at the indices 2, 3, and 5. The smallest index that is filled with 10 is 2. Therefore, we return 2.
+ 
+
+Constraints:
+
+1 <= index, number <= 109
+At most 105 calls will be made in total to change and find.
+*/
+
+
+//
+
+import java.util.*;
+
+class NumberContainers {
+    private HashMap<Integer, Integer> ind; // Maps index -> number
+    private HashMap<Integer, PriorityQueue<Integer>> num; // Maps number -> min-heap of indices
+
+    public NumberContainers() {
+        ind = new HashMap<>();
+        num = new HashMap<>();
+    }
+
+    public void change(int index, int number) {
+        if (ind.containsKey(index)) {
+            int prevNumber = ind.get(index);
+            if (prevNumber != number) { // Only remove if the number actually changed
+                num.get(prevNumber).offer(-index); // Lazy deletion by marking it
+            }
+        }
+        
+        ind.put(index, number);
+        num.computeIfAbsent(number, k -> new PriorityQueue<>()).offer(index);
+    }
+
+    public int find(int number) {
+        PriorityQueue<Integer> pq = num.get(number);
+        if (pq == null || pq.isEmpty()) return -1;
+
+        while (!pq.isEmpty() && ind.getOrDefault(pq.peek(), -1) != number) {
+            pq.poll(); // Remove stale indices
+        }
+        
+        return pq.isEmpty() ? -1 : pq.peek();
+    }
+}
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index, number);
+ * int param_2 = obj.find(number);
+ */
