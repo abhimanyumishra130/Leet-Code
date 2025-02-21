930f9cb97fd4e7c206b77f800be7c854d68ee2ad
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2025-02-21 12:22:40 +0530

    Create 1261 Find Elements in a Contaminated Binary Tree.java

---
+++ problems/1261 Find Elements in a Contaminated Binary Tree.java
@@ -0,0 +1,164 @@
+/*
+Given a binary tree with the following rules:
+
+root.val == 0
+For any treeNode:
+If treeNode.val has a value x and treeNode.left != null, then treeNode.left.val == 2 * x + 1
+If treeNode.val has a value x and treeNode.right != null, then treeNode.right.val == 2 * x + 2
+Now the binary tree is contaminated, which means all treeNode.val have been changed to -1.
+
+Implement the FindElements class:
+
+FindElements(TreeNode* root) Initializes the object with a contaminated binary tree and recovers it.
+bool find(int target) Returns true if the target value exists in the recovered binary tree.
+ 
+
+Example 1:
+
+
+Input
+["FindElements","find","find"]
+[[[-1,null,-1]],[1],[2]]
+Output
+[null,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1]); 
+findElements.find(1); // return False 
+findElements.find(2); // return True 
+Example 2:
+
+
+Input
+["FindElements","find","find","find"]
+[[[-1,-1,-1,-1,-1]],[1],[3],[5]]
+Output
+[null,true,true,false]
+Explanation
+FindElements findElements = new FindElements([-1,-1,-1,-1,-1]);
+findElements.find(1); // return True
+findElements.find(3); // return True
+findElements.find(5); // return False
+Example 3:
+
+
+Input
+["FindElements","find","find","find","find"]
+[[[-1,null,-1,-1,null,-1]],[2],[3],[4],[5]]
+Output
+[null,true,false,false,true]
+Explanation
+FindElements findElements = new FindElements([-1,null,-1,-1,null,-1]);
+findElements.find(2); // return True
+findElements.find(3); // return False
+findElements.find(4); // return False
+findElements.find(5); // return True
+ 
+
+Constraints:
+
+TreeNode.val == -1
+The height of the binary tree is less than or equal to 20
+The total number of nodes is between [1, 104]
+Total calls of find() is between [1, 104]
+0 <= target <= 106
+
+*/
+
+
+//
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+     private TreeNode root;
+    public FindElements(TreeNode root) {
+        this.root = root;
+        this.root.val = 0;
+    }
+    
+    public boolean find(int target) {
+        TreeNode temp = this.root;
+        return findTarget(temp, target);
+    }
+
+    private boolean findTarget(TreeNode root, int target){
+        if(root == null) return false;
+        if(target == root.val) return true;
+        
+        if(root.left != null){
+            root.left.val = 2*root.val+1;
+        }
+        if(root.right != null){
+            root.right.val = 2*root.val+2;
+        }
+        return findTarget(root.left, target) || findTarget(root.right, target);
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
+
+
+
+
+
+//Optimized
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class FindElements {
+     private TreeNode root;
+     private HashSet<Integer> set;
+    public FindElements(TreeNode root) {
+        set = new HashSet<>();
+        set.add(0);
+        this.root = root;
+        makeTree(this.root, 0);
+    }
+
+    private void makeTree(TreeNode root, int val){
+        if(root == null) return;
+        root.val = val;
+        this.set.add(val);
+        if(root.left != null) makeTree(root.left, 2*root.val+1);
+        if(root.right != null) makeTree(root.right, 2*root.val+2);
+    }
+    
+    public boolean find(int target) {
+        return set.contains(target);
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements obj = new FindElements(root);
+ * boolean param_1 = obj.find(target);
+ */
